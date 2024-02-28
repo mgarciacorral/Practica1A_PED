bd658e020cc5a222ddbec031092599de266fa9df
Matias Garcia Corral <matihontayt@gmail.com>
2024-02-28 12:47:35 +0100

    menu hecho practica terminada

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,6 @@
-import EstructurasDatos.Lineales.LEG;
-import EstructurasDatos.Lineales.LEGConUltimo;
-
 public class Main {
     public static void main(String[] args)
     {
-        LEGConUltimo<Integer> lista = new LEGConUltimo<>();
-
-        for(int i = 0; i < 1; i++)
-        {
-            lista.append(i);
-        }
-
-        lista.toStringIterativo();
-        lista.remove(0);
-        lista.toStringInversoIterativo();
-        lista.toStringRecursivo();
-        lista.toStringInversoRecursivo();
-        System.out.println("Elemento en la posicion 5: " + lista.get(5));
+        Menu menu = new Menu();
     }
 }
--- src/Menu.java
+++ src/Menu.java
@@ -1,53 +1,127 @@
 import EstructurasDatos.Lineales.*;
-
-import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Menu{
-
-
-    public Menu(){
+public class Menu
+{
+    LEGConUltimo<Integer> lista;
+    public Menu()
+    {
+        this.lista = new LEGConUltimo<Integer>();
+        ejecutar();
     }
 
-
-    public void mostrarMenu(){
-        System.out.println("\tMENÚ LEGConUltimo");
-        System.out.println("1.-Crear lista de elementos enteros");
-        System.out.println("2.-Añadir elementos a la lista");
-        System.out.println("3.-Listado directo de elementos(iterativos)");
-        System.out.println("4.-Listado inverso de elementos(recursivos)");
-        System.out.println("6.-Listado inverso de elementos(iterativos)");
-        System.out.println("7.-Listado inverso de elementos(recursivos)");
-        System.out.println("0.-Salir");
-    }
-
-    public static void crearLista(){
-
-        LEG lista = new LEGConUltimo<Integer>();
-        System.out.println("**LEGConUltimo vacía creada**");
-        pulsaIntro();
-
-
-    }
-
-    public static void pulsaIntro(){
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Pulse enter para continuar...");
-        scanner.nextLine();
-        scanner.close();
-    }
-
-    /*public static void añadirElementos(E elemento){
-
-        System.out.println("\tAÑADIR ELEMENTOS A LA LISTA");
-        System.out.println("Introduzca claves (-1 para terminar):");
-        Scanner scanner = new Scanner(System.in);
-        int clave = scanner.nextInt();
-        while (clave != -1)
+    public void ejecutar()
+    {
+        Scanner sc = new Scanner(System.in);
+        int opcion = -1;
+        while (opcion != 0)
         {
-            lista.append(clave);
-            clave = scanner.nextInt();
+            System.out.println("\t\tMENU\n");
+            System.out.println("1. Crear nueva lista de elementos enteros(se borrara la anterior)");
+            System.out.println("2. Añadir elementos a la lista");
+            System.out.println("3. Listado de elementos directo iterativo");
+            System.out.println("4. Listado de elementos directo recursivo");
+            System.out.println("5. Listado de elementos inverso iterativo");
+            System.out.println("6. Listado de elementos inverso recursivo");
+            System.out.println("0. Salir");
+            System.out.print("\nIntroduce una opcion: ");
+            opcion = sc.nextInt();
+            switch (opcion)
+            {
+                case 1:
+                    crearLista();
+                    break;
+                case 2:
+                    anadirElementos();
+                    break;
+                case 3:
+                    directoIterativo();
+                    break;
+                case 4:
+                    directoRecursivo();
+                    break;
+                case 5:
+                    inversoIterativo();
+                    break;
+                case 6:
+                    inversoRecursivo();
+                    break;
+                case 0:
+                    System.out.println("Saliendo...");
+                    break;
+                default:
+                    System.out.println("Opcion no valida");
+                    break;
+            }
         }
-    }*/
+    }
+
+    private void pulseIntro()
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Pulse intro para continuar");
+        sc.nextLine();
+    }
+
+    private void crearLista()
+    {
+        System.out.println("\t\tCREAR LISTA\n");
+        lista = new LEGConUltimo<Integer>();
+        System.out.println("**Lista creada**\n");
+        pulseIntro();
+    }
+
+    private void anadirElementos()
+    {
+        System.out.println("\t\tAÑADIR ELEMENTOS\n");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Introduce elementos a añadir y -1 para terminar: ");
+        int elemento = sc.nextInt();
+        while (elemento != -1)
+        {
+            lista.append(elemento);
+            elemento = sc.nextInt();
+        }
+
+        System.out.println("Actualmente la lista es: ");
+        lista.toStringIterativo();
+        System.out.println();
+        pulseIntro();
+    }
+
+    private void directoIterativo()
+    {
+        System.out.println("\t\tLISTADO DE ELEMENTOS DIRECTO ITERATIVO\n");
+        System.out.print("La lista es: ");
+        lista.toStringIterativo();
+        System.out.println();
+        pulseIntro();
+    }
+
+    private void directoRecursivo()
+    {
+        System.out.println("\t\tLISTADO DE ELEMENTOS DIRECTO RECURSIVO\n");
+        System.out.print("La lista es: ");
+        lista.toStringRecursivo();
+        System.out.println();
+        pulseIntro();
+    }
+
+    private void inversoIterativo()
+    {
+        System.out.println("\t\tLISTADO DE ELEMENTOS INVERSO ITERATIVO\n");
+        System.out.print("La lista es: ");
+        lista.toStringInversoIterativo();
+        System.out.println();
+        pulseIntro();
+    }
+
+    private void inversoRecursivo()
+    {
+        System.out.println("\t\tLISTADO DE ELEMENTOS INVERSO RECURSIVO\n");
+        System.out.print("La lista es: ");
+        lista.toStringInversoRecursivo();
+        System.out.println();
+        pulseIntro();
+    }
 }
